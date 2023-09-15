2a6ee7324718de5390b838e882ad01ae48a02887
moeinparvizi <giventulip@gmail.com>
2023-09-15 14:09:45 +0330

    create navigation bar , create setting click

---
+++ src/assets/js/script.js
@@ -0,0 +1,6 @@
+
+
+document.querySelector('.setting-user-button').addEventListener('click', () => {
+  const sub = document.querySelector('.setting-user-subbutton')
+  sub.classList.toggle('transform-none')
+})
--- tailwind.config.js
+++ tailwind.config.js
@@ -3,6 +3,15 @@ module.exports = {
   content: ["./src/**/*.{html,js}"],
   theme: {
     extend: {},
+    colors: {
+      'airbnb': {
+        pink: '#ff385c',
+        white: '#fff',
+        dark: '#222',
+        gray: '#BFBFBF',
+        graywhite: '#ddd',
+      },
+    },
   },
   plugins: [],
 }
